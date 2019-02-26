c167fe4f4382324c17d032bbf6580841d0cb8e4e
Kybox <nslr@riseup.net>
2019-02-26 12:58:23 +0100

    Initial commit

---
+++ src/main/java/fr/kybox/kytasks/KytasksApplication.java
@@ -0,0 +1,13 @@
+package fr.kybox.kytasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KytasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KytasksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/kybox/kytasks/KytasksApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.kybox.kytasks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KytasksApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
